2130415b730140cda880e1ee03204ac01fbe3480
Mads <mads61096@gmail.com>
2021-09-27 11:13:43 +0200

    Add project files.

---
+++ Football/FootballPlayer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Football
+{
+    public class FootballPlayer
+    {
+        private string _name;
+        private int _price;
+        private int _shirtNumber;
+        public static int NextId = 0;
+        public int Id { get; }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (value.Length < 4) { throw new ArgumentOutOfRangeException(); }
+                _name = value;
+            }
+        }
+
+        public int Price
+        {
+            get => _price;
+            set
+            {
+                if (value <= 0) { throw new ArgumentOutOfRangeException(); }
+                _price = value;
+            }
+        }
+
+        public int ShirtNumber
+        {
+            get => _shirtNumber;
+            set
+            {
+                if (value < 1 || value > 100) { throw new ArgumentOutOfRangeException(); }
+                _shirtNumber = value;
+            }
+        }
+
+        public FootballPlayer(string name, int price, int shirtNumber)
+        {
+            Name = name;
+            Price = price;
+            ShirtNumber = shirtNumber;
+            Id = NextId;
+            NextId++;
+        }
+    }
+}
---
+++ FootballTests/FootballPlayerTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Football;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Football.Tests
+{
+    [TestClass()]
+    public class FootballPlayerTests
+    {
+        [TestMethod()]
+        public void FootballPlayerTest()
+        {
+            FootballPlayer player = new FootballPlayer("Mads", 1700, 6);
+            FootballPlayer player2 = new FootballPlayer("Mads", 1700, 6);
+            Assert.AreEqual("Mads", player.Name);
+            Assert.AreEqual(1700, player.Price);
+            Assert.AreEqual(6, player.ShirtNumber);
+            Assert.AreEqual(0, player.Id);
+            Assert.AreEqual(1, player2.Id);
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(
+                () => player.Name = "iii");
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(
+                () => player.Price = 0);
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(
+                () => player.ShirtNumber = 0);
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(
+                () => player.ShirtNumber = 101);
+        }
+    }
+}
